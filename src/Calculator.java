6a6b2e3807135908469fdad81f8624b3c1750b2d
Ben Salem Med Ali <medalibensalem18@gmail.com>
2024-02-16 15:15:26 +0100

    Creation d'un fichier java

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+	float x,y;
+	public static float addition(int x,int y){
+		float res;
+		res=x+y;
+		return (res);
+	}
+	public static float soustraction(float x,float y){
+		float res;
+		res=x-y;
+		return(res);
+	}
+	public static float multiplication(float x,float y){
+		float res;
+		res=x*y;
+		return(res);
+	}
+	public static float division(float x,float y){
+		float res;
+		res=x/y;
+		return(res);
+	}
+	public static void main(String[]args){
+		float x;
+		System.out.println("donner la valeur de x : ");
+		
+	}
+}
